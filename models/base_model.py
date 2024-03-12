7a96b04291d608e032d1487152562652ab9e1004
Doctware <yusclever9@gmail.com>
2024-03-12 10:37:48 +0100

    The base model

---
+++ models/base_model.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+import datetime
+import uuid
+"""
+This module contains the Baseclass
+"""
+
+
+class BaseModel:
+    """ the BaseClass Module """
+
+    def __init__(self):
+        """ inisializing public instance attribute """
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.datetime.now() 
+        self.updated_at = datetime.datetime.now()
+
+    def __str__(self):
+        """ Print some information about BaseModes """
+        return "[{}] ({}) {}"\
+            .format(self.__class__.__name__, self.id, self.__dict__)
+
+    def save(self):
+        """ Updates the public instance attribute U
+        pdated_at
+            with the current date time
+        """
+        self.updated_at = datetime.datetime.now()
+
+    def to_dict(self):
+        """
+        returns a dictionary containing all keys/value of __dict__
+        of instances
+        """
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return obj_dict
