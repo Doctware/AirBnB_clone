2893d949c3bb0d67fa7790aeba098440d990dc6c
Doctware <yusclever9@gmail.com>
2024-03-21 11:03:19 +0100

    The base model

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,7 @@
 """ This module contains the class BaseModel """
 from datetime import datetime
 import uuid
+from models import storage 
 
 
 class BaseModel:
@@ -20,12 +21,13 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
+            storage.new(self)
 
     def save(self):
         """ update the public insatnce attribute to the
             current date time"""
-
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """ returns a dictionary containing all keys/values of
