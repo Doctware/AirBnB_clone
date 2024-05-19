66db954512215f8ac91f999dfdc4aefa5c7bbdf3
Doctware <yusclever9@gmail.com>
2024-05-19 23:38:02 +0100

    Constructing the BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -10,12 +10,12 @@ class BaseModel:
     def __init__(self):
         """ the init method """
         self.id = str(uuid.uuid4())
-        created_at = datetime.datetime.now()
-        updated_at = datetime.datetime.now()
+        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
 
     def __str__(self):
         """ Print BaseModel information """
-        return "[{}] ({}) <{}>"\
+        return "[{}] ({}) {}"\
             .format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
