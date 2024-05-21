ffd331129be33977e2fa842dde97e30ea244aa6a
Doctware <yusclever9@gmail.com>
2024-05-21 13:35:47 +0100

    building the base model

--- models/base_model.py
+++ models/base_model.py
@@ -8,15 +8,34 @@ import datetime
 class BaseModel:
     """ The class BaseModel thats future classs
         inherit from """
-    def __init__(self):
+
+    def __init__(self, *args, **kwargs):
         """ the init method
             assign uniqe id to public instace attribute 'id'
 
             assign current datetime to public instance atribute 'created_at'
             assign current datetime to public instance atribute 'updated_at'
+            
+            args: (Unused arguments)
+
+            Using kwargs:
+                check if kwargs is not empty if so....
+                each key of this dictionary is an attribute name.
+                each value of this dictionary is the value
+                of the attribute name
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
+        # do this if kwargs is not called
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now().isoformat()
+
+        # do this if kwargs is called
+        for key, value in kwargs.items():
+            if key != "__class__":
+                if key in ['created_at', 'updated_at']:
+                    value = datetime.datetime.now().isoformat()
+                setattr(self, key, value)
+
         self.updated_at = datetime.datetime.now()
 
     def __str__(self):
