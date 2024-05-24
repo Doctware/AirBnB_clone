fb90ecf4b3f65dab80c8b920355e73b8e83267fb
Doctware <yusclever9@gmail.com>
2024-05-25 00:19:29 +0100

    The User info

---
+++ models/user.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" this module contains the user class """
+from base_model import BaseModel
+
+
+class User(BaseMode):
+    """ the class user thats inherits from BaseModel """
+
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
