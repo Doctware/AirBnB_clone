536ac7271bd12afe4e77a415bb5c7ebe95cf756c
Doctware <yusclever9@gmail.com>
2024-05-24 07:25:00 +0100

    The console

--- console.py
+++ console.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """ this module contain class HBNBCommand class """
 import cmd
+from models.base_model import BaseModel
+from models import storage
 
 
 class HBNBCommand(cmd.Cmd):
@@ -25,6 +27,29 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """ Handle empty line input (just ENTER)."""
         pass
+
+    def do_create(self, arg):
+        """ Create: this command create instance of BaseModel
+            save it to (the JSON file) and print the id
+            Ex: $ create BaseModel """
+
+        class_name = arg.strip() # Remove leading/trailing white spase
+
+        if not class_name:
+            print("** class name missing **")
+        else:
+            try:
+                class_obj = {"BaseModel": BaseModel}
+                if class_name not in class_obj:
+                    raise KeyError("** class dosen't exist **")
+
+                new_obj = class_obj[class_name]()
+                storage.new(new_obj)
+                storage.save()
+                print("{}".format(new_obj.id))
+            except KeyError as e:
+                print(e)
+
 
 
 if __name__ == "__main__":
