31767e7452fd03e0da569b5cad9047d0017b8396
Doctware <yusclever9@gmail.com>
2024-05-30 17:21:57 +0100

    retreving Test model

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+""" Test port for the BaseModel """
+from models.base_model import BaseModel
+from datetime import datetime
+from unittest import TestCase
+import uuid
+
+
+class TestBaseModel(TestCase):
+    """ The class TestBaseModel to test the BaseModel """
+
+    def setUp(self):
+        self.bm = BaseModel()
+        self.bm.name = "My First Model"
+        self.bm.number = 89
+
+    def test_PubIstAtt(self):
+        """ this method is testing the public insatnce attribute """
+        self.assertEqual(len(self.bm.id), 36)
+        self.assertIsInstance(self.bm.id, str)
+        self.assertIsInstance(self.bm.created_at, datetime)
+        self.assertIsInstance(self.bm.created_at, datetime)
+
+    def test_save(self):
+        """ this method is testing the save method """
+        original_created_at = self.bm.created_at
+        self.bm.save()
+        self.assertGreater(self.bm.updated_at, original_created_at)
+
+    def test_to_dict(self):
+        """ this method is testing to_dict method """
+        data = self.bm.to_dict()
+        self.assertIsInstance(data, dict)
+        self.assertIn('id', data)
+        self.assertEqual(len(self.bm.id), 36)
+        self.assertIsInstance(data['id'], str)
+        self.assertIn('created_at', data)
+        self.assertIsInstance(data['created_at'], str)
+        self.assertIn('updated_at', data)
+        self.assertIsInstance(data['updated_at'], str)
+        self.assertIn('__class__', data)
+        self.assertEqual(data['__class__'], self.bm.__class__.__name__)
+        self.assertEqual(data['name'], 'My First Model')
+        self.assertEqual(data['number'], 89)
+
+    def test_str(self):
+        """ this method test string output """
+        output = "[BaseModel] ({}) {}".\
+            format(self.bm.id, self.bm.__dict__)
+        self.assertEqual(str(self.bm), output)
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ tests/test_models/test_user.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+""" this module contains test port for User """
+from models.user import User
+import unittest
+import uuid
+from datetime import datetime
+
+
+class UserTestCase(unittest.TestCase):
+    """ the class UserTestCase """
+
+    def setUp(self):
+        """ the setup method """
+        self.us = User()
+
+    def test_use_instance(self):
+        """ testing user instance """
+        self.assertIsInstance(self.us, User)
+
+    def test_user_attribute(self):
+        """ testing user attribute """
+        self.assertEqual(self.us.last_name, "")
+        self.assertEqual(self.us.first_name, "")
+        self.assertEqual(self.us.email, "")
+        self.assertEqual(self.us.password, "")
+
+    def test_user_id(self):
+        """ testing user id """
+        self.assertIsInstance(self.us.id, str)
+        self.assertTrue(len(self.us.id) > 0)
+
+    def test_user_created_at_updated_at(self):
+        """ testing user created_at and updated_at attr """
+        self.assertIsInstance(self.us.created_at, datetime)
+        self.assertIsInstance(self.us.created_at, datetime)
+
+    def test_user_str(self):
+        """ testing __str__ method user """
+        string = str(self.us)
+        self.assertIn("[User] ({})".format(self.us.id), string)
+        self.assertIn("'id': '{}'".format(self.us.id), string)
+
+       
+    def test_user_to_dict(self):
+        """Test the to_dict method of User."""
+        us_dict = self.us.to_dict()
+        self.assertEqual(us_dict['__class__'], 'User')
+        self.assertEqual(us_dict['__class__'], User)
+        self.assertEqual(us_dict['last_name'], "")
+        self.assertEqual(us_dict['first_name'], "")
+        self.assertEqual(us_dict['email'], "")
+        self.assertEqual(us_dict['password'], "")
+        self.assertEqual(us_dict['first_name'], "")
+        self.assertEqual(us_dict['last_name'], "")
+        self.assertIsInstance(us_dict['created_at'], str)
+        self.assertIsInstance(us_dict['updated_at'], str)
+        self.assertEqual(us_dict['created_at'], self.us.created_at.isoformat())
+        self.assertEqual(us_dict['updated_at'], self.us.updated_at.isoformat())
+ 
+
+
+if __name__ == "__main__":
+    unittest.main()
